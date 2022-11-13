e4c37f765f03eb89d7ebb8b44a134a3a6005c3c8
kayos@tcp.direct <kayos@tcp.direct>
2022-11-13 04:14:17 -0800

    Dedupe socks and verify connectivity

--- sox.go
+++ sox.go
@@ -13,6 +13,9 @@ func persistentResolver(hostname string) []netip.Addr {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	var ips []netip.Addr
+	if hostname == "" {
+		return ips
+	}
 	for n := 0; n < 5; n++ {
 		var err error
 		var res []netip.Addr
@@ -33,6 +36,8 @@ func (c *Checker) GetSOCKS() (sox []netip.AddrPort, err error) {
 	if err = c.Update(); err != nil {
 		return
 	}
+	var tmpMap = make(map[netip.AddrPort]struct{})
+	var tmpMapMu = &sync.RWMutex{}
 	wg := &sync.WaitGroup{}
 	for _, serv := range c.m {
 		wg.Add(1)
@@ -45,14 +50,59 @@ func (c *Checker) GetSOCKS() (sox []netip.AddrPort, err error) {
 					port = 1080
 				}
 				ap := netip.AddrPortFrom(ip, port)
-				if ap.IsValid() && ap.Port() > 0 {
+				tmpMapMu.RLock()
+				_, ok := tmpMap[ap]
+				if ap.IsValid() && ap.Port() > 0 && !ok {
 					sox = append(sox, ap)
+					tmpMapMu.RUnlock()
+					tmpMapMu.Lock()
+					tmpMap[ap] = struct{}{}
+					tmpMapMu.Unlock()
 					continue
 				}
-				err = fmt.Errorf("invalid address/port combo: %s", ap.String())
+				tmpMapMu.RUnlock()
+				if !ap.IsValid() {
+					err = fmt.Errorf("invalid address/port combo: %s", ap.String())
+				}
 			}
 		}(&serv)
 	}
 	wg.Wait()
 	return
+}
+
+func (c *Checker) GetAndVerifySOCKS() (chan netip.AddrPort, chan error) {
+	sox, err := c.GetSOCKS()
+	var errs = make(chan error, len(sox)+1)
+	var verified = make(chan netip.AddrPort, len(sox))
+	if err != nil || len(sox) == 0 {
+		errs <- err
+		close(errs)
+		return nil, errs
+	}
+	wg := &sync.WaitGroup{}
+	wg.Add(len(sox))
+	for _, prx := range sox {
+		time.Sleep(250 * time.Millisecond)
+		go func(prx netip.AddrPort) {
+			defer wg.Done()
+			var conn net.Conn
+			conn, err = net.DialTimeout("tcp", prx.String(), 5*time.Second)
+			if err != nil {
+				errs <- err
+			}
+			if conn != nil {
+				_ = conn.Close()
+			}
+			if err == nil {
+				verified <- prx
+			}
+		}(prx)
+	}
+	go func() {
+		wg.Wait()
+		close(errs)
+		close(verified)
+	}()
+	return verified, errs
 }
--- sox_test.go
+++ sox_test.go
@@ -6,16 +6,35 @@ import (
 
 func TestChecker_GetSOCKS(t *testing.T) {
 	c := NewChecker()
-	if err := c.Update(); err != nil {
-		t.Fatalf("%s", err.Error())
-	}
-	gotSox, err := c.GetSOCKS()
-	if err != nil {
-		t.Error(err)
-	}
-	if len(gotSox) == 0 {
-		t.Error("expected non-zero length")
-	}
-	t.Logf("got %d socks", len(gotSox))
-	t.Logf("%v", gotSox)
+	t.Run("GetSOCKS", func(t *testing.T) {
+		if err := c.Update(); err != nil {
+			t.Fatalf("%s", err.Error())
+		}
+		gotSox, err := c.GetSOCKS()
+		if err != nil {
+			t.Error(err)
+		}
+		if len(gotSox) == 0 {
+			t.Error("expected non-zero length")
+		}
+		t.Logf("got %d socks", len(gotSox))
+		t.Logf("%v", gotSox)
+	})
+
+	t.Run("GetAndVerifySOCKS", func(t *testing.T) {
+		if err := c.Update(); err != nil {
+			t.Fatalf("%s", err.Error())
+		}
+		gotSox, errs := c.GetAndVerifySOCKS()
+		for sox := range gotSox {
+			select {
+			case err := <-errs:
+				if err != nil {
+					t.Error(err)
+				}
+			default:
+				t.Logf("got verified: %s", sox.String())
+			}
+		}
+	})
 }
